25c31261ae9754f1964dcc4149d848c091873c48
Kunal <kunaldhir@live.in>
2014-07-28 12:33:51 +0530

    Create users.php

---
+++ users.php
@@ -0,0 +1,13 @@
+<?php
+include 'connection.php';
+
+echo "<h2>User Data</h2>";
+$result=mysql_query("SELECT * FROM data ") or die('Invalid query: ' . mysql_error());	
+    while($data=mysql_fetch_array($result))  {
+		echo "<div>Username : ".$data['username']."<br><br>";
+		echo "Email ID : ".$data['email']."<br><br>";
+		echo "Password : ".$data['password']."<br><br>";		
+		echo"<button>Edit</button>";
+		echo"<button>Delete</button><br><br><br><br></div>";}
+
+?>
