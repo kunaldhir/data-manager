8384f309824f26577c032eb6686f3da46d50c4a8
Kunal <kunaldhir@live.in>
2014-07-30 15:03:21 +0530

    Update members.php

--- members.php
+++ members.php
@@ -1,3 +1,37 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="//code.jquery.com/ui/1.11.0/jquery-ui.js"></script>
+<style>
+#form {display: none ;
+	   border: solid;}
+</style>
+
+<script>
+$(document).ready(function() {
+$( "#form" ).dialog({
+autoOpen: false,
+show: {
+effect: "fade",
+duration: 0
+},
+hide: {
+effect: "fade",
+duration: 0
+}
+});
+$( "#click" ).click(function() {
+$( "#form" ).dialog( "open" );
+});
+});
+</script>
+
+ <script>
+$(function() { $( "#tags" ).autocomplete({
+source: search.php
+});
+});
+</script>
+      
 <?php
 session_start();
 global $start;
@@ -5,15 +39,13 @@ global $expire;
 if(!isset($_SESSION['id'])){header('Location:login.php');}
 
     $start=$_SESSION['start'];
-	echo 'start:'.$start;
-	echo 'expire:'.$expire;
-	$diff=($expire-$start)/60*60;
-	if($diff>10)
+	$expire=time();
+	$diff=($expire-$start)/60;
+	if($diff>5)
 	{ 
 		session_destroy();
       echo '<script>alert("your session has been expired");window.location.reload();</script>';
-	 
-	  
+	 	  
 	}
 
 	$_SESSION['start']=time();
@@ -44,15 +76,26 @@ if(isset($_POST["search"])) {
 
 $searchtext = $_POST['searchbox'];
 
-$result=mysql_query("SELECT * FROM contacts WHERE username='$username' LIKE '%$searchtext%'") or die('Invalid query: ' . mysql_error());
-	$contacts=mysql_fetch_array($result);
+$result=mysql_query("SELECT * FROM contacts WHERE username='$username' and  name LIKE '$searchtext%'") or die('Invalid query: ' . mysql_error());
+$num= mysql_num_rows($result);
+if ($num>=1) {
 	
-	echo "Name : ".$contacts["name"]."<br><br>";
-	echo "Mobile : ".$contacts["mobile"]."<br><br>";
+	while($contacts=mysql_fetch_array($result)){
+	
+	echo "<div id='click'>Name : ".$contacts["name"]."<br><br></div>";
+/*	echo "Mobile : ".$contacts["mobile"]."<br><br>";
 	echo"<button id='edit'>Edit</button>";
-	echo"<button id='delete'>Delete</button><br><br>";
+	echo"<button id='delete'>Delete</button><br><br>";*/
 }
-
-?>
+}
+else {echo "no result";}
+}?>
+<div id="form">
+<form method='post' action='<?php echo$_SERVER["PHP_SELF"];?>'>
+New name : <input type="text" name="newname" id="newname"><br><br>
+New Mobile No. : <input type:"text" name="newmobile" id="newmobile"><br><br>
+<input type="submit" value="Make Changes" id="submit" name="submit">
+</form>
+</div>
 <a href="add.php">Add new</a><br><br>
 <a href="logout.php">Logout</a>
