6f8a734e6e1eaa2b2eca35b53358f724c306a2ad
Kunal <kunaldhir@live.in>
2014-07-30 17:28:43 +0530

    Create search.php

---
+++ search.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include 'connection.php';
+$searchtext = trim(strip_tags($_GET['term']));
+$username=$_SESSION['username'];
+$result=mysql_query("SELECT * FROM contacts WHERE username='$username' and  name LIKE '$searchtext%'") or die('Invalid query: ' . mysql_error());
+$num= mysql_num_rows($result);
+if ($num>=1) {
+	
+	while($contacts=mysql_fetch_array($result)){
+	
+		$contacts['value']=htmlentities(stripslashes($contacts['name']));
+		$contacts['id']=(int)$contacts['id'];
+		$array[] = $contacts;
+
+}
+echo json_encode($array);
+}
+?>
